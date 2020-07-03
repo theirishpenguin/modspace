79817fa60f1fe5eaf31f1dceab4398f29cad3dbc
declan@weuseopensource.com <declan@weuseopensource.com>
2020-07-03 23:12:32 +0100

    Added spec for defining module under an explicit class.

--- spec/modspace_spec.rb
+++ spec/modspace_spec.rb
@@ -4,12 +4,20 @@ RSpec.describe Modspace do
   end
 
   describe "::def_mod" do
-    it "define the nested module Foo::Bar:Zoo" do
+    it "define the nested module Foo::Bar:Zoo within the current execution context" do
       zoo_module = Modspace.def_mod(self, 'Foo', 'Bar', 'Zoo')
 
       # Note: current_namespace is RSpec::ExampleGroups::Modspace::DefMod in these specs
       expect(RSpec::ExampleGroups::Modspace::DefMod::Foo::Bar::Zoo).to be_a(Module)
       expect(RSpec::ExampleGroups::Modspace::DefMod::Foo::Bar::Zoo).to eql(zoo_module)
+    end
+
+    it "define the nested module Foo::Bar:Zoo within an explicit module/class" do
+      zoo_module = Modspace.def_class_mod(Object, 'Foo', 'Bar', 'Zoo')
+
+      # Note: current_namespace is RSpec::ExampleGroups::Modspace::DefMod in these specs
+      expect(Foo::Bar::Zoo).to be_a(Module)
+      expect(Foo::Bar::Zoo).to eql(zoo_module)
     end
   end
 end
